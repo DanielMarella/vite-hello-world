1becbf5eb5619838aaa0d9a440f285dc1ee00b78
DanielMarella <marella.daniel96@gmail.com>
2023-06-14 17:26:42 +0200

    fix some style

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -4,7 +4,7 @@
 
 
 <template>
-    <div class="header">
+    <div class="header rainbow-border">
         <h1 class="rainbow-text">La mia prima app con Vite!!!!!!!</h1>
     </div>
 
@@ -33,4 +33,21 @@
         border: 10px solid purple;
         background-color: black;
     }
+
+
+    @-webkit-keyframes rainbow {
+    0% {border-color: #fd004c;}
+    15% {border-color: #fe9000;}
+    30% {border-color: #fff020;}
+    45% {border-color: #3edf4b;}
+    60% {border-color: #3363ff;}
+    75% {border-color: #b102b7;}
+    100% {border-color: hsl(255, 100%, 50%);}
+    }
+
+    .rainbow-border{
+    border: 10px solid hsl(0, 100%, 50%);
+    -webkit-animation: rainbow 2s infinite alternate;
+    }
+
 </style>
