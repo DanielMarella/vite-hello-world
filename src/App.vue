49dc0590ffa38b5a3349af2c324dd2c6af482e54
DanielMarella <marella.daniel96@gmail.com>
2023-06-14 17:24:46 +0200

    add a new component name AppHero

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script >
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+
 
 export default{
   name : 'MainApp',
   components : {
-    AppTitle
+    AppTitle,
+    AppHero,
   }
 }
 
@@ -12,6 +15,7 @@ export default{
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
 
 <style scoped>
