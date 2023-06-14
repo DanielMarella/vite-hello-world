383302fb556f3bba8b8a2540907d50b7f40ac89b
DanielMarella <marella.daniel96@gmail.com>
2023-06-14 17:05:09 +0200

    add a title and some css for pretty visualization

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,10 +1,36 @@
 <script>
+
 </script>
 
 
 <template>
+    <div class="header">
+        <h1 class="rainbow-text">La mia prima app con Vite!!!!!!!</h1>
+    </div>
+
+
 </template>
 
 
-<style>
+<style scoped>
+
+    h1{
+        font-size: 3rem;
+    }
+
+    .rainbow-text {
+    background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red); 
+    background-clip: text;
+    color: transparent;
+    }
+
+    .header{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 50rem;
+        height: 10rem;
+        border: 10px solid purple;
+        background-color: black;
+    }
 </style>
